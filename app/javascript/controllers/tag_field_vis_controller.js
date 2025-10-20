a7c81baf36b0746e591986c29b1997ed69c082ce
Louis Bourne <chifury@proton.me>
2025-10-20 17:32:10 +0900

    styling

--- app/javascript/controllers/tag_field_vis_controller.js
+++ app/javascript/controllers/tag_field_vis_controller.js
@@ -1,25 +1,24 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="tag-field-vis"
 export default class extends Controller {
-static targets = ["tag"]
+  static targets = ["tag"];
 
   connect() {
-    console.log("tag controller connected")
-    this.visibleCount = 1
-    this.updateTagVisibility()
+    console.log("tag controller connected");
+    this.visibleCount = 1;
+    this.updateTagVisibility();
   }
 
   hiddenVisibleTags(event) {
     // console.log(event)
-    this.visibleCount = Math.min(this.visibleCount + 1, this.tagTargets.length)
-    this.updateTagVisibility()
+    this.visibleCount = Math.min(this.visibleCount + 1, this.tagTargets.length);
+    this.updateTagVisibility();
   }
 
   updateTagVisibility() {
     // console.log(this.tagTargets)
     this.tagTargets.forEach((element, index) => {
-
       const destroyInputTag = element.querySelector('input[name*="_destroy"]');
       console.log(destroyInputTag);
 
@@ -30,6 +29,6 @@ static targets = ["tag"]
         element.setAttribute("hidden", "");
         if (destroyInputTag) destroyInputTag.value = "1";
       }
-    })
+    });
   }
 }
