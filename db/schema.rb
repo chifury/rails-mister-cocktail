ba31375045426cf1bf0fcdd0889d4b890b2d71f3
Louis Bourne <chifury@proton.me>
2025-11-07 16:19:49 +0700

    feat: added Profile model with terminal command.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_05_072946) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_07_091913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_05_072946) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.text "bio"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -93,6 +101,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_05_072946) do
   add_foreign_key "doses", "ingredients"
   add_foreign_key "favorites", "cocktails"
   add_foreign_key "favorites", "users"
+  add_foreign_key "profiles", "users"
   add_foreign_key "tags", "cocktails"
   add_foreign_key "user_reviews", "cocktails"
   add_foreign_key "user_reviews", "users"
