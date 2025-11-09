f66f292bc1cf8be61aeea3517271e4d9f8f2d947
Louis Bourne <chifury@proton.me>
2025-11-09 13:34:49 +0700

    feat: added logic for profile model.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_many :points, dependent: :destroy
+
+  has_many :profile_badges, dependent: :destroy
+  has_many :badges, through: :profile_badges
+
+  has_many :profile_achievements, dependent: :destroy
+  has_many :achievements, through: :profile_achievements
 end
