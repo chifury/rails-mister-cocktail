3fe08839d2002912ff962d80c69ff30a2a4e7078
Louis Bourne <chifury@proton.me>
2025-11-09 13:38:56 +0700

    feat: added logic for badge model.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,7 @@
 class Badge < ApplicationRecord
+  has_many :profile_badges, dependent: :destroy
+  has_many :profiles, through: :profile_badges
+
+  validates :name, presence: true
+  validates :level_required, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
