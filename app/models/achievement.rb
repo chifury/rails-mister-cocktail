f8eb4886cf3cb25faf47f4fc2bd3033f50f552a5
Louis Bourne <chifury@proton.me>
2025-11-09 13:40:44 +0700

    feat: added logic for achievement model.

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,2 +1,6 @@
 class Achievement < ApplicationRecord
+  has_many :profile_achievements, dependent: :destroy
+  has_many :profiles, through: :profile_achievements
+
+  validates :name, presence: true
 end
