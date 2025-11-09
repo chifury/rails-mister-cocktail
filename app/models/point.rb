68802ca2bec22c2370f46660fe1c55fb9da7429b
Louis Bourne <chifury@proton.me>
2025-11-09 13:36:34 +0700

    feat: added logic for point model.

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,3 +1,6 @@
 class Point < ApplicationRecord
   belongs_to :profile
+
+  validates :xp, numericality: { greater_than_or_equal_to: 0 }
+  validates :level, numericality: { greater_than_or_equal_to: 1 }
 end
